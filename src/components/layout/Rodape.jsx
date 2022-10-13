575feea105ce8093092fb4488d040ac3513a7a76
Vinils76 <antoniovinicius7670@gmail.com>
2022-10-13 11:49:40 -0300

    eventos

--- src/components/layout/Rodape.jsx
+++ src/components/layout/Rodape.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
 const Rodape = () => {
+  const [estilos, setEstilos] = useState({
+    textalign: "center",
+    textTransform: "uppercase",
+    borde: "solid 2px red",
+  });
+
+  const esconder = () => setEstilos({ display: "none " });
+  const exibir = () => setEstilos({ display: "block " });
+
   return (
-    <footer>
-      <h2>Rodapé</h2>
-    </footer>
+    <>
+      <p>
+        <span onMouseOver={esconder}>Esconder</span> |
+        <span onMouseOver={exibir}>Exibir</span>
+      </p>
+      <footer
+        style={estilos}
+        onMouseOver={() => setEstilos({ display: "none" })}
+      >
+        <h2>Rodapé</h2>
+      </footer>
+    </>
   );
 };
 
