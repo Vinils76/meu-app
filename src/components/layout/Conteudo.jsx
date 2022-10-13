4534787cabe93799cb6ab8005dbcea96046f170d
Vinils76 <antoniovinicius7670@gmail.com>
2022-10-13 11:34:12 -0300

    contador

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import Artigo from "../Artigo";
 
 const Conteudo = () => {
+  const [corFundo, setCorFundo] = useState();
+  const [contador, setContador] = useState(0);
+
+  const fundoAmarelo = () => setCorFundo("#FFEE71");
+  const fundoAzul = () => setCorFundo("lightblue");
+  const fundoVermelho = () => setCorFundo("lightpink");
+
+  const atualizaContagem = () => setContador(contador + 1);
+
   return (
-    <main>
+    <main style={{ backgroundColor: corFundo }}>
+      <button onMouseOver={fundoAmarelo}>Amarelo</button>
+      <button onMouseOver={fundoAzul}>Azul</button>
+      <button onMouseOver={fundoVermelho}>Vermelho</button>
+
       <section>
-        <h2>Conteúdo do site</h2>
+        <h2 onClick={atualizaContagem}>{contador}</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
           laboriosam totam fuga sed voluptatibus quasi error amet provident
