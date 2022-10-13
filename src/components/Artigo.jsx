e2a584e0b501ccf4fe30c36448150254fce5aa6d
Vinils76 <antoniovinicius7670@gmail.com>
2022-10-13 09:29:47 -0300

    Versão usando desestruturação

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,10 +1,23 @@
-const Artigo = (props) => {
+// VERSÃO TRADICIONAL PROPS
+/* const Artigo = (props) => {
   return (
     <article>
       <h3> {props.titulo} </h3>
       <h4> {props.subtitulo} </h4>
       <h4> {props.curso} </h4>
       <p> {props.children} </p>
+    </article>
+  );
+}; */
+
+// VERSÃO DESESTRUTURADA
+const Artigo = ({ titulo, subtitulo, curso, children }) => {
+  return (
+    <article>
+      <h3> {titulo} </h3>
+      <h4> {subtitulo} </h4>
+      <h4> {curso} </h4>
+      <p> {children} </p>
     </article>
   );
 };
